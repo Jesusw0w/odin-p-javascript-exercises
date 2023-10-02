9ecfd1e44d1b5178c8c048224281c5b942d7df75
Jesusw0w <70320342+Jesusw0w@users.noreply.github.com>
2023-10-02 21:01:39 +0100

    Exercise 1 and 2  completed
    Hello World and Repeat String problems solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(text,multiple) {
+    
+    stringMultiplied = ''
+    
+    for(let i=0; i < multiple; i++ ){
+        stringMultiplied += text
+    }
 
+    return stringMultiplied
 };
 
 // Do not edit below this line
